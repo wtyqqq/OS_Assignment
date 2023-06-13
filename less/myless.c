a65cbcfc7417f62264e514fa3197670c45032a36
Tianyi Wang <tianyiwang58@gmail.com>
2023-06-13 22:45:14 +0800

    improve less

--- less/myless.c
+++ less/myless.c
@@ -4,51 +4,55 @@
 #include <unistd.h>
 #include <sys/mman.h>
 #include <string.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <termios.h>
-
-// 输出一页内容，返回页尾偏移量
-int one_page(char *s, int d)
+#include <sys/ioctl.h>
+int height;
+int display_page(char *ptr, int offset)
+// 打印一整页
 {
-    int i, count = 0;
-    char *buffer = malloc(1024); // 使用动态内存分配空间
+    int index, count = 0;
+    char *buf = malloc(1024); 
 
-    s += d; // 定位至指定偏移量处
-    for (i = 0; i < 1024; i++)
+    ptr += offset; // 定位至指定偏移量处
+    for (index = 0; index < 1024; index++)
     {
-        if (s[i] == '\n')
+        if (ptr[index] == '\n')
             count++; // 计算行数
-        if (count == 10)
+        if (count == height)
             break; // 显示第 7 行时停止
     }
-    memcpy(buffer, s, i); // 将内容复制到缓冲区
-    buffer[i] = '\0'; // 添加字符串结束符
+    memcpy(buf, ptr, index); // 将内容复制到缓冲区
+    buf[index] = '\0';     // 添加字符串结束符
     system("clear");
-    printf("%s\n", buffer);
-    return i;
+    printf("%s\n", buf);
+    printf("(u/U 1 line up, d/D 1 line down, Enter one screen):");
+    return index;
 }
 
+
+int next_row(char *ptr, int offset)
 // 下翻一页，返回页尾偏移量
-int next_row(char *s, int d)
 {
-    int i,count = 0,j;
+    int index, count = 0, j;
     char *buffer = malloc(1024); // 使用动态内存分配空间
-    s += d; // 定位至指定偏移量处
-    for (j = 0; d - j > 0; j--)
+    ptr += offset;                      // 定位至指定偏移量处
+    for (j = 0; offset - j > 0; j--)
     {
-        if (s[j] == '\n')
+        if (ptr[j] == '\n')
             count++; // 计算行数
-        if (count == 10)
+        if (count == height)
             break; // 找到上一页的行末位置
     }
-    for (i = 0; i < 100; i++)
-        if (s[i] == '\n')
-            break; // 找到下一行的行末位置
-    memcpy(buffer, s + j + 1, i - j); // 将内容复制到缓冲区
-    buffer[i - j - 1] = '\0'; // 添加字符串结束符
+    for (index = 0; index < 100; index++)
+        if (ptr[i] == '\n')
+            break;                    // 找到下一行的行末位置
+    memcpy(buffer, ptr + j + 1, index - j); // 将内容复制到缓冲区
+    buffer[index - j - 1] = '\0';         // 添加字符串结束符
     system("clear");
     printf("%s\n", buffer);
-    return i;
+    printf("(u/U 1 line up, d/D 1 line down, Enter one screen):");
+    return index;
 }
 
 // 上翻一页，返回页首偏移量
@@ -56,7 +60,7 @@ int last_row(char *s, int d)
 {
     int count = 0;
     char *buffer = malloc(1024);
-    int i = d,j = d;
+    int i = d, j = d;
 
     for (; i > 0; i--)
     {
@@ -65,76 +69,92 @@ int last_row(char *s, int d)
         if (count == 2)
             break; // 找到上一页的行末位置
     }
-    
-    count = 0;    
+
+    count = 0;
     for (; j > -1; j--)
     {
         if (s[j] == '\n')
             count++; // 计算行数
-        
-        if (count == 12)
+
+        if (count == height + 2)
             break; // 找到上一页的行末位置
     }
-    if (count < 11)
+    if (count < height + 1)
     {
-        memcpy(buffer, s , d); // 将内容复制到缓冲区
-    	buffer[d - 1] = '\0'; // 添加字符串结束符    
-    	system("clear");
-    	printf("%s\n", buffer);	
-    	return d - 1;
+        memcpy(buffer, s, d); // 将内容复制到缓冲区
+        buffer[d - 1] = '\0'; // 添加字符串结束符
+        system("clear");
+        printf("%s\n", buffer);
+        printf("(u/U 1 line up, d/D 1 line down, Enter one screen):");
+        return d - 1;
     }
     else
     {
         memcpy(buffer, s + j + 1, i - j); // 将内容复制到缓冲区
-    	buffer[i - j - 1] = '\0'; // 添加字符串结束符
-    	system("clear");
+        buffer[i - j - 1] = '\0';         // 添加字符串结束符
+        system("clear");
         printf("%s\n", buffer);
+        printf("(u/U 1 line up, d/D 1 line down, Enter one screen):");
         return i;
     }
-
-
 }
 
-int main()
+int main(int argc, char *argv[])
 {
-    int fd, play = 0;
-    char lab;
+    int fd, now = 0;
+    char choice;
     char *start;
-    struct stat sb;
+    struct stat file_stat;
+    // 获取终端高度
+    struct winsize size;
+    ioctl(STDIN_FILENO, TIOCGWINSZ, &size);
+    height = size.ws_row - 1;
+    // 干掉烦人的缓冲区
     struct termios original, raw;
-    tcgetattr(STDIN_FILENO, &original);  // 获取当前终端属性
+    tcgetattr(STDIN_FILENO, &original); // 获取当前终端属性
     raw = original;
-    raw.c_lflag &= ~(ICANON | ECHO);     // 禁用标准模式和回显
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);  // 设置原始模式
-    // 打开文件并将其映射至内存
-    fd = open("../2.txt", O_RDONLY);
-    fstat(fd, &sb);
-    start = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (start == MAP_FAILED)
-        return (1);
-
-    // 输出第一页内容
-    play = one_page(start, play) + 1;
-    lab = getchar();
-    while (lab != 'q' && lab != 'Q')
+    raw.c_lflag &= ~(ICANON | ECHO);          // 禁用标准模式和回显
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw); // 设置原始模式
+    if (argc != 2)
     {
-        if (play > sb.st_size) // 如果超出文件大小则退出
+        printf("usage：%s <filename>\n", argv[0]);
+        exit(1);
+    }
+    char *filename = argv[1];
+
+    // 打开文件
+    fd = open(filename, O_RDONLY);
+    // 获取文件状态
+    fstat(fd, &file_stat);
+    // 映射
+    start = mmap(NULL, file_stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (start == MAP_FAILED)
+    {
+        perror("mmap failed!!!!!!!!!");
+        exit(1);
+    }
+    // 输出第一页内容
+    now = display_page(start, now) + 1;
+    choice = getchar();
+    while (choice != 'q' && choice != 'Q')
+    {
+        if (now > file_stat.st_size) // 如果超出文件大小则退出
         {
-            lab = getchar();
+            choice = getchar();
             break;
         }
-        else if (lab == ' ' ) // 空格键下翻一页
-            play = play + 1 + one_page(start, play);
-        else if (lab == 'D' || lab == 'd') // D 或 d 键下翻一行
-            play = play + 1 + next_row(start, play);
-        else if (lab == 'U' || lab == 'u') // U 或 u 键上翻一页
-            play = 1 + last_row(start, play);
-        lab = getchar();
+        else if (choice == ' ' || choice == '\n') // 空格键h或者换行下翻一页
+            now = now + 1 + display_page(start, now);
+        else if (choice == 'D' || choice == 'd') // D 或 d 键下翻一行
+            now = now + 1 + next_row(start, now);
+        else if (choice == 'U' || choice == 'u') // U 或 u 键上翻一页
+            now = 1 + last_row(start, now);
+        choice = getchar();
     }
 
     // 解除内存映射并关闭文件
-    munmap(start, sb.st_size);
+    munmap(start, file_stat.st_size);
     close(fd);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original);  // 恢复终端属性
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original); // 恢复终端属性
     return 0;
 }
